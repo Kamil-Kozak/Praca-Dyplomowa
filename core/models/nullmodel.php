ca53457718503a58552c40d5ed47019655630d16
Kamil Kozak <rydel.poczta@gmail.com>
2018-01-17 19:07:08 +0100

    Pusty model

---
+++ core/models/nullmodel.php
@@ -0,0 +1,15 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: Kamil Kozak
+ * Date: 17.01.2018
+ * Time: 16:14
+ */
+
+class nullmodel{
+    public function __construct($model)
+    {
+        return $model;
+    }
+}
+?>
