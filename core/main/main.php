19e1cd1e44764530a31644d11a2480ab5424f9f9
Kamil Kozak <rydel.poczta@gmail.com>
2018-01-17 18:41:33 +0100

    Rejestr - Globalny dostęp do obiektów z zastosowaniem wzorca singleton - poprawiony błąd w linii 12

--- core/main/main.php
+++ core/main/main.php
@@ -9,7 +9,7 @@
 class main{
     static public function _templateLoader($controller, $template){
         $config = registry::register("config");
-        $templatefile = $config->view_path.$controller."/".$template."php";
+        $templatefile = $config->view_path.$controller."/".$template.".php";
 
         if(file_exists($templatefile)){
             include_once($templatefile);
