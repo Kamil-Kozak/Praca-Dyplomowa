b0c19870c750ea2c8f621accbf3b1b5f97f97e93
Kamil Kozak <rydel.poczta@gmail.com>
2018-01-17 12:40:10 +0100

    Rejestr - Globalny dostęp do obiektów z zastosowaniem wzorca singleton

---
+++ core/main/registry.php
@@ -0,0 +1,30 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: Kamil Kozak
+ * Date: 16.01.2018
+ * Time: 19:12
+ */
+
+class registry
+{
+    private static $registered = array();
+
+    public static function register($object, $params = false)
+    {
+        if (empty(self::$registered[$object]))
+        {
+            if (!params)
+            {
+                self::$registered[$object] = new $object();
+            }
+            else
+            {
+                self::$registered[$object] = new $object($params);
+            }
+        }
+
+        return self::$registered[$object];
+    }
+}
+?>
